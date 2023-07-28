6f85f95b276eb05e0fef8e31d15f5c7eaeaa02cf
Tavkel <gmininb@gmail.com>
2023-07-28 17:09:03 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_230726_stream/Homework230726StreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_230726_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework230726StreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework230726StreamApplication.class, args);
+    }
+
+}
